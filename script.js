ede75034178005c81ebee609bdcd409fbf772f24
Dav1ik <111747295+David-Ivanov@users.noreply.github.com>
2023-11-02 17:57:30 +0200

    finish

--- script.js
+++ script.js
@@ -1,8 +1,75 @@
 const btn = document.querySelector(".portfolio-btn");
-const visibleList = document.querySelector(".portfolio-visible-list");
+const btnText = document.querySelector(".portfolio-btn-text");
+const hiddenBox = document.querySelector(".hidden-list-box");
 const hiddenList = document.querySelector(".portfolio-hidden-list");
 
 btn.addEventListener('click', () => {
-    hiddenList.classList.toggle("hidden");
-    visibleList.classList.toggle("margin-bottom");
+    hiddenBox.classList.toggle("hidden");
+    hiddenList.classList.toggle("hidden-list");
+    changeBtnText();
 });
+
+function changeBtnText() {
+    switch (btnText.textContent) {
+
+        case "Другие работы":
+            btnText.textContent = "Скрыть";
+            break;
+
+        default:
+            btnText.textContent = "Другие работы";
+            break;
+    }
+}
+
+// scroll 
+
+const reviewsList = document.querySelector(".reviews-list");
+const scrollToLeftBtn = document.querySelector(".scroll-left");
+const scrollToRightBtn = document.querySelector(".scroll-right");
+
+let score = 0;
+
+function checkMinScore() {
+    if (score < 0) {
+        score = 0;
+    }
+}
+function checkMaxScore() {
+    if (score > 2000) {
+        score = 2000;
+    }
+}
+
+scrollToLeftBtn.addEventListener('click', () => {
+    score -= 500;
+    reviewsList.scroll({
+        left: score,
+        behavior: "smooth",
+    });
+    checkMinScore();
+});
+scrollToRightBtn.addEventListener('click', () => {
+    score += 500;
+    reviewsList.scroll({
+        left: score,
+        behavior: "smooth",
+    });
+    checkMaxScore();
+});
+
+// quetions 
+
+const quetionItems = document.querySelectorAll(".quetions-item");
+const quetionBtns = document.querySelectorAll(".quetions-btn");
+const quetionIcons = document.querySelectorAll(".quetions-icon");
+const quetionText = document.querySelectorAll(".quetions-text");
+
+for (let i = 0; i < quetionBtns.length; i++) {
+    quetionBtns[i].addEventListener('click', () => {
+        quetionIcons[i].classList.toggle("quetions-icon-active");
+        quetionItems[i].classList.toggle("quetions-item-active");
+        quetionText[i].classList.toggle("quetions-text-active");
+    });
+}
+
